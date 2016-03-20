43b6b46037d65da75a10fd3f274b5091f22d546a
LucasMW <great-lucas@hotmail.com>
2016-03-20 00:15:44 -0300

    first commit

---
+++ Assets/code/Probe.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public enum Type
+{
+	basic,
+	mining,
+	research,
+	combat,
+	booster,
+	duplicator
+};
+public class Probe  
+{
+	public Type type;
+	public uint grade;
+
+	public virtual double MiningMultiplier()
+	{
+		return 1.0 + (grade - 1) / 10.0;
+	}
+	public virtual double RevenueMultiplier()
+	{
+		return 1.0 + (grade - 1) / 10.0;
+	}
+
+}
---
+++ Assets/code/Site.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public enum Ranking
+{
+	E,
+	D,
+	C,
+	B,
+	A,
+	S
+};
+public class Site  //Miranium site
+{
+	public Ranking mining;
+	public Ranking revenue;
+	public Ranking combatSupport;
+	public string name; //site indentifier
+
+	public Probe placedProbe;
+
+	public bool reached; // if the player has ever reached this site and placed a probe in it 
+
+
+	public Site(string name, Ranking mining, Ranking revenue, Ranking combatSupport)
+	{
+		this.name = name;
+		this.mining = mining;
+		this.revenue = revenue;
+		this.combatSupport = combatSupport;
+	}
+
+
+}
